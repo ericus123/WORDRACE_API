39f8b7a4bc87766006b502cedd8abddfc9dc29ef
AMANI Eric <amaniericus@gmail.com>
2021-10-23 12:44:30 -0400

    ch(setup):setup initial project

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-const app = express();
-
-const port = 3000 || process.env.PORT;
-
-app.listen(port,() => {
-    console.log("listening on port " + port);
-});
---
+++ src/routes/index.js
@@ -0,0 +1,9 @@
+import Router from "express";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "../../swagger.json";
+
+
+const router = Router();
+router.use("/api-docs", swaggerUi.serve);
+router.get("/api-docs", swaggerUi.setup(swaggerDocument));
+export default router;
